572aaea369098c710fb14d3a15609a35d47be86b
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-07-01 10:18:32 +0900

    Create SelectionSort.py

---
+++ Algorithm/SelectionSort.py
@@ -0,0 +1,13 @@
+#선택정렬 - 2중 for문과 big O 표기법 상 시간 같음.
+
+arr = [10, 9, 11, 7, 5, 2, 6]
+length = len(arr)
+
+for j in range(length - 1):
+    minimum = j
+    for i in range(j,length):
+        if( arr[minimum] > arr[i] ):
+            minimum = i
+    arr[minimum], arr[j] = arr[j], arr[minimum]
+
+arr
