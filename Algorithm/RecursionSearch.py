ab479b7a399d25edcc4230dcfbb057e00fff5823
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-07-01 09:42:16 +0900

    Update RecursionSearch.py

--- Algorithm/RecursionSearch.py
+++ Algorithm/RecursionSearch.py
@@ -1,17 +1,15 @@
 lst = [0, 1, 2, 3, 4, 6,7,8,9,24,33,55,66,99]
 
-want = 99
-start = 0
-end = len(lst)-1
+want = 99 #change this
 
-def search(start, end, want):
+def search(lst, start, end, want):
     key = int((start + end) / 2)
     if(lst[key] < want):
-        return search(key+1, end, want)
+        return search(lst, key+1, end, want)
     elif(lst[key] > want):
-        return search(start, key-1, want)
+        return search(lst,start, key-1, want)
     elif(lst[key] == want):
         print('찾았다, ',key+1,'번째에 있습니다')
         return key
 
-search(start, end, want)
+search(lst, 0, len(lst)-1 , want)
