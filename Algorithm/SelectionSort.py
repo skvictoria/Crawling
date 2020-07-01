b242a26170a878685ccb401886b65deabee1eb84
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-07-01 10:23:12 +0900

    Update SelectionSort.py

--- Algorithm/SelectionSort.py
+++ Algorithm/SelectionSort.py
@@ -1,13 +1,19 @@
+import matplotlib.pyplot as plt
+
 #선택정렬 - 2중 for문과 big O 표기법 상 시간 같음.
 
 arr = [10, 9, 11, 7, 5, 2, 6]
 length = len(arr)
 
+plt.bar(list(range(len(arr))), arr)
+plt.show()
 for j in range(length - 1):
     minimum = j
     for i in range(j,length):
-        if( arr[minimum] > arr[i] ):
+        if( arr[minimum] < arr[i] ):
             minimum = i
     arr[minimum], arr[j] = arr[j], arr[minimum]
+    plt.bar(list(range(len(arr))), arr)
+    plt.show()
 
 arr
