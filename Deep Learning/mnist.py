154943a9cb28745c3eec6408af42c6fce4f634fa
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-07-08 16:02:01 +0900

    Create mnist.py

---
+++ Deep Learning/mnist.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+# data loading
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# data preprocessing
+train_images = train_images.reshape((60000, 28*28))
+test_images = test_images.reshape((10000, 28*28))
+
+# one-hot encoding
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# model loading
+model = models.Sequential()
+model.add(layers.Dense(units = 512,activation = 'relu', input_shape = (28*28, )))
+model.add(layers.Dense(units = 10, activation='softmax'))
+model.summary()
+
+# model compile
+model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+history = model.fit(train_images, train_labels, epochs=30, batch_size=128, validation_split=0.2)
+
+# model evaluate
+test_loss, test_acc = model.evaluate(x = test_images, y = test_labels)
+predict = model.predict(test_images[0].reshape((1, 28*28)))
+
+print('predict probability : ', predict)
+print('predict number : ', np.argmax(predict))
+
+plt.figure()
+plt.imshow(test_images[0].reshape(28, 28)*255)
+plt.show()
